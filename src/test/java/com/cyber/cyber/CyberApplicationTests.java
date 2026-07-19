d19e30860e2c2b440ab192552e4f46487ab10dc1
Nilesh <nilesh2126vp@gmail.com>
2026-07-19 20:42:57 +0530

    Initial Sentinel AI project

---
+++ src/test/java/com/cyber/cyber/CyberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyber.cyber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
